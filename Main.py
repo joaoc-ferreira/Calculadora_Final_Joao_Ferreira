2d97052ae16b969d4b9826ca0f8aecf326fab9a3
joaoc-ferreira <jferreira93@hotmail.com>
2025-01-23 11:40:08 +0000

    Update v2 Calculadora
    Adicionei a subtracção, bem como, a multiplicação e divisão simples.

--- Main.py
+++ Main.py
@@ -4,3 +4,6 @@
         result = num1 * num2
     elif operador == '/':
         result = num1 / num2
+        "-": lambda: num1 - num2,
+        "X": lambda: num1 * num2,
+        "/": lambda: num1 / num2
